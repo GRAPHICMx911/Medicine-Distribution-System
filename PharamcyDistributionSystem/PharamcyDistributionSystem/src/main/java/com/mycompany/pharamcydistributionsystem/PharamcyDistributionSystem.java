d76f13f11715106e1f30e93d580461df0b6d619b
GRAPHICMx911 <graphicmx911@gmail.com>
2024-01-22 23:14:08 +0500

    first commit

---
+++ PharamcyDistributionSystem/PharamcyDistributionSystem/src/main/java/com/mycompany/pharamcydistributionsystem/PharamcyDistributionSystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pharamcydistributionsystem;
+
+/**
+ *
+ * @author GraphicMx
+ */
+public class PharamcyDistributionSystem {
+
+    public static void main(String[] args) {
+      
+    }
+}
